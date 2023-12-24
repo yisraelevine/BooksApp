13d9181bccddc23de47e88a727dbcbeb1aad762f
yisraelevine <yisraelevine@gmail.com>
2023-12-24 02:17:16 -0500

    Fixing navigation bugs

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -7,45 +7,45 @@ import { data } from './interfaces';
 })
 export class GlobalService {
 	constructor(private http: HttpClient) { }
-	base: HTMLBaseElement | null = document.querySelector('base');
+	base: HTMLBaseElement | null = document.querySelector('base')
 	data: data = {
 		children: undefined,
 		sidebar: [],
 		page: undefined,
 		tree: [],
 		navigation: undefined
-	};
-	deepestOpenedId: number | undefined;
-	topOpenedId: number | undefined;
-	isLoading = false;
+	}
+	deepestOpenedId: number | undefined
+	topOpenedId: number | undefined
+	isLoading = false
 	getData(id: number) {
-		this.isLoading = true;
+		this.isLoading = true
 		this.http.get<data>('api', {
 			params: { id },
 			responseType: 'json'
 		}).subscribe({
 			next: (data: data) => {
 				if (data.tree.length === 0) {
-					history.pushState(null, '', '');
-					this.changeBase('');
-					document.title = 'ספריית ליובאוויטש';
+					this.navigate()
+					document.title = 'ספריית ליובאוויטש'
 				}
 				else {
-					history.pushState(null, '', id.toString());
-					this.changeBase(id.toString());
-					document.title = data.tree[data.tree.length - 1].heading + ' - ספריית ליובאוויטש';
+					this.navigate(id.toString())
+					document.title = data.tree[data.tree.length - 1].heading + ' - ספריית ליובאוויטש'
 				}
-				this.data = data;
-				this.topOpenedId = data.tree[0]?.id;
-				this.deepestOpenedId = data.tree.length > 1 ? data.tree[1].id : this.topOpenedId;
+				this.data = data
+				this.topOpenedId = data.tree[0]?.id
+				this.deepestOpenedId = data.tree.length > 1 ? data.tree[1].id : this.topOpenedId
 			},
 			complete: () => this.isLoading = false
-		});
+		})
 	}
 	removeHtmlCode(htmlCode: string): string {
-		return new DOMParser().parseFromString(htmlCode, 'text/html').body.textContent || '';
+		return new DOMParser().parseFromString(htmlCode, 'text/html').body.textContent || ''
 	}
-	changeBase(newBase: string) {
-		if (this.base !== null) this.base.href = newBase
+	navigate(path?: string) {
+		path = './' + (path || '')
+		history.pushState(null, '', path)
+		if (this.base !== null) this.base.href = path
 	}
 }
