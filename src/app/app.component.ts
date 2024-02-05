89e32e461decd8a26bfc4debb6eb021f00f41465
yisraelevine <yisraelevine@gmail.com>
2024-02-04 23:32:53 -0500

    Fixing bug and optimizing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,21 @@ export class AppComponent implements OnInit {
     ngOnInit() {
         window.addEventListener('popstate', () =>
             this.global.getMain(+window.location.pathname.replace(/\D/g, ''), true, true))
+            
         this.global.getMain(+window.location.pathname.replace(/\D/g, ''), true).add(() => {
             const url = new URL(window.location.href)
-            this.global.search_form_data.phrase = url.searchParams.get('phrase') || ''
-            this.global.search_form_data.section_id = +(url.searchParams.get('section_id') || '')
-            if (url.pathname.includes('search')) {
-                if (idInSections(this.global.search_form_data.section_id, this.global.main.sections))
-                    this.global.getSearch()
-                else this.global.getStatistics()
-            }
+            const phrase = url.searchParams.get('phrase') || ''
+
+            if (!url.pathname.includes('search') || !phrase) return
+
+            const section_id = +(url.searchParams.get('section_id') || '')
+            const is_section = idInSections(section_id, this.global.main.sections)
+            
+            this.global.search_form_data.phrase = phrase
+            this.global.search_form_data.section_id = is_section ? section_id : 0
+
+            if (is_section) this.global.getSearch()
+            else this.global.getStatistics()
         })
     }
 }
