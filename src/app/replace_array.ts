5351355b1cdc0c8a5a8a5ca9861de07352c43f55
yisraelevine <yisraelevine@gmail.com>
2024-02-28 15:03:02 -0500

    Fixing inconsistencies and adding items to replace_array

--- src/app/replace_array.ts
+++ src/app/replace_array.ts
@@ -1,25 +1,31 @@
 export default [
-    ["[small_begin]", "<span class=small>"],
-    ["[small_end]", "</span>"],
+    ['[small_begin]', '<span class=small>'],
+    ['[small_end]', '</span>'],
 
-    ["[smallitalic_begin]", "<span class=smallitalic>"],
-    ["[smallitalic_end]", "</span>"],
+    ['[smallitalic_begin]', '<span class=smallitalic>'],
+    ['[smallitalic_end]', '</span>'],
 
-    ["[cup]", "<span class=cup>"],
-    ["[/cup]", "</span>"],
+    ['[cup]', '<span class=cup>'],
+    ['[/cup]', '</span>'],
 
-    ["[hagoho]", "<div class=hagoho>"],
-    ["[/hagoho]", "</div>"],
+    ['[hagoho]', '<div class=hagoho>'],
+    ['[/hagoho]', '</div>'],
 
-    ["[kitzur]", "<p class=kitzur>"],
-    ["[/kitzur]", "</p>"],
+    ['[kitzur]', '<p class=kitzur>'],
+    ['[/kitzur]', '</p>'],
 
-    ["[kitzur_begin]", "<p class=kitzur>"],
-    ["[kitzur_end]", "</p>"],
+    ['[kitzur_begin]', '<p class=kitzur>'],
+    ['[kitzur_end]', '</p>'],
 
-    ["[dibur_maschil]", "<b>"],
-    ["[/dibur_maschil]", "</b>"],
+    ['[dibur_maschil]', '<b>'],
+    ['[/dibur_maschil]', '</b>'],
 
-    ["[quotation]", '\"'],
-    ["[/quotation]", '\"']
+    ['[quotation]', '"'],
+    ['[/quotation]', '"'],
+
+    ['[headingintext_begin]', '<h4>'],
+    ['[headingintext_end]', '</h4>'],
+
+    ['[zman_begin]', '<h5>'],
+    ['[zman_end]', '</h5>']
 ]
